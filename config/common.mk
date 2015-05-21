271c4331be3942b7c73d360cf967821c69c7fef5
Mattia D'Alleva <antaresone@antaresone.eu>
2015-05-21 03:31:38 +0200

    common: disable ADB authentication
    Needed to run adbd at boot
    
    Change-Id: Id4c886b3f4d7a4302ffc6545af95310e93d35480

--- config/common.mk
+++ config/common.mk
@@ -26,10 +26,8 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 endif
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-# Enable ADB authentication
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
-endif
+# Disable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 
 ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
   PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
